0f6076012a100e321d47e0823f74e91b989e3578
Ian Owira <kivlov02@gmail.com>
2018-03-16 14:53:23 +0100

    created whitespace between component and }

--- client/components/ApiConsole.js
+++ client/components/ApiConsole.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { Layout, Stack, Card, TextField, Button } from '@shopify/polaris';
